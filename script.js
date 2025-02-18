5d548d707f97ee6dcefb3fcb6854e97765d10e23
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-02-18 20:04:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,26 @@
 class Rectangle{
-    constructor(Rwidth, Rheight){
-this.Rheight = Rheight;
-this.Rwidth = Rwidth;
-    }
-    get width(){
-        return this.Rwidth;
-    }
-    get height(){
-        return this.Rheight;
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
     }
     getArea(){
-
-        return this.width*this.height;
-    }
-    set width(newW){
-        this.Rwidth = newW;
-    }
-    set height(newH){
-        this.Rheight = newH;
+        return this.width * this.height;
     }
 }
 
 class Square extends Rectangle{
     constructor(side){
         super(side, side);
-
     }
-    
-
-    getperimeter(){
-return 4*this.width;
+    getPerimeter(){
+        return 4 * this.width;
     }
 }
 
 const rect = new Rectangle(5,6);
 console.log(rect.getArea());
 const sq = new Square(4);
-console.log(sq.getperimeter());
+console.log(sq.getPerimeter());
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
